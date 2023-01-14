f3fa6c559931a99bc4dc5ed344ac0d4648b26f8f
Duke Romkey <dukeromkey@gmail.com>
2023-01-13 21:07:46 -0600

    Placeholder content on page

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -16,17 +16,24 @@ function NFTDropPage() {
         </div>
       </div>
       {/* right  */}
-      <div>
+      <div className=" flex flex-1 flex-col p-12 lg:col-span-6">
         {/* Header */}
-        <header>
+        <header className="flex items-center justify-between">
           <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
             The <span className="font-extrabold underline decoration-pink-600/50">Duke Romkey</span> NFT Market Place
           </h1>
           <button className="rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base">Sign In</button>
         </header>
-        {/* Content */}
 
+        <hr className="my-2 border"/>
+        {/* Content */}
+        <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
+          <img className="w-80 object-cover pb-10 lg:h-40" src="https://links.papareact.com/bdy" alt="" />
+          <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">The DUKE NFT CLUB</h1>
+          <p className="pt-2 text-xl text-green-500">13/21 NFT's claimed</p>
+        </div>
         {/* Mint Button */}
+        <button className="h-16 w-full bg-red-600 text-white rounded-full mt-10 font-bold">Mint NFT (0.01 ETH)</button>
       </div>
 
     </div>

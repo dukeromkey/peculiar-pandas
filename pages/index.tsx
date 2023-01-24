cb5cbd86de810979bcbec0fcb62c2646c8c7df5e
Duke Romkey <dukeromkey@gmail.com>
2023-01-23 20:28:45 -0600

    index project name change

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home = () => {
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900;8..144,1000&display=swap" rel="stylesheet" />
       </Head>
       <h1 className="mb-10 text-4xl font-extralight">
-        <span className="font-extrabold">Duke's</span> NFT Market Place
+        <span className="font-extrabold">Duke's</span> NFT Projects
       </h1>
 
       <main className='bg-slate-100 p-10 shadow-xl shadow-rose-400/20'>
